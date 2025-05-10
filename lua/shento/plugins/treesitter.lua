0e8352dc99822f07dc41a0ffb16a71335206b2b3
Shento Hendriks <shentohendriks@gmail.com>
2025-05-10 15:57:27 +0200

    fixed the bug where autotag didn't work

--- lua/shento/plugins/treesitter.lua
+++ lua/shento/plugins/treesitter.lua
@@ -1,57 +1,57 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+  -- nvim-treesitter configuration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup({
+        highlight = {
+          enable = true,
         },
-      },
-    })
-  end,
+        indent = { enable = true },
+        -- NOTE: The 'autotag' option here is removed or commented out
+        -- because nvim-ts-autotag now handles its own setup entirely.
+        -- autotag = {
+        --   enable = true, -- REMOVE OR COMMENT OUT THIS BLOCK
+        -- },
+        ensure_installed = {
+          "json", "javascript", "typescript", "tsx", "yaml", "html", "css",
+          "prisma", "markdown", "markdown_inline", "svelte", "graphql",
+          "bash", "lua", "vim", "dockerfile", "gitignore", "query", "vimdoc", "c",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+      })
+    end,
+  },
+
+  -- nvim-ts-autotag configuration (this part should already be similar to what you had)
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Good to keep for load order
+    event = { "BufReadPost", "BufNewFile" }, -- Or ft = {"html", "xml", ...} or "VeryLazy"
+                                         -- Using BufReadPost can be slightly safer to ensure Treesitter is ready
+    config = function()
+      require('nvim-ts-autotag').setup({
+        -- Add any nvim-ts-autotag specific options here if needed.
+        -- Often, an empty setup call is sufficient.
+        -- Example:
+        -- filetypes = { "html", "xml", "javascriptreact", "typescriptreact", "svelte", "vue" },
+        -- enable_rename = true,
+        -- enable_close = true,
+        -- enable_close_on_slash = true,
+      })
+      print("nvim-ts-autotag setup called") -- Add this for debugging
+    end,
+  }
 }
+
