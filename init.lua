9eb42f63bf778d08669be436e747ce396da1b1f8
Shento Hendriks <shentohendriks@gmail.com>
2025-05-11 16:24:58 +0200

    saved settings

--- init.lua
+++ init.lua
@@ -21,7 +21,18 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-local plugins = {}
+local plugins = {
+	{"catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
+}
 local opts = {}
 -- Setup lazy.nvim
 require("lazy").setup(plugins, opts)
+local builtin = require('telescope.builtin')
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
