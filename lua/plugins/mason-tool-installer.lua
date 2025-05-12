c3c344df17ff33f9c451d8e744c29f6b5b40bef7
Shento Hendriks <shentohendriks@gmail.com>
2025-05-12 02:36:36 +0200

    fixed double lsp servers

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -1,32 +1,22 @@
 return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  dependencies = { "williamboman/mason.nvim" },
-  opts = {
-    ensure_installed = {
-      -- LSP servers
-      "lua_ls",
-      "cssls",
-      "ts_ls",
-      "emmet_ls",
-      "html",
-      "volar",
-      "tailwindcss",
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	dependencies = { "williamboman/mason.nvim" },
+	opts = {
+		ensure_installed = {
+			-- Formatters
+			"prettier", -- JS, TS, CSS, HTML
+			"stylua", -- Lua
+			"black", -- Python
 
-      -- Formatters
-      "prettier",   -- JS, TS, CSS, HTML
-      "stylua",     -- Lua
-      "black",      -- Python
-
-      -- Linters
-      "eslint_d",   -- Javascript
-      "flake8",     -- Python
-      -- add others as needed
-    },
-    auto_update = true,
-    run_on_start = true,
-  },
-  config = function(_, opts)
-    require("mason-tool-installer").setup(opts)
-  end,
+			-- Linters
+			"eslint_d", -- Javascript
+			"flake8", -- Python
+			-- add others as needed
+		},
+		auto_update = true,
+		run_on_start = true,
+	},
+	config = function(_, opts)
+		require("mason-tool-installer").setup(opts)
+	end,
 }
-
