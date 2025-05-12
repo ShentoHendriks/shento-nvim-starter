97cd54a0997981da1ae4d373dd963db15679c490
Shento Hendriks <shentohendriks@gmail.com>
2025-05-12 10:32:59 +0200

    tried to make svelte working

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -1,33 +1,34 @@
 return {
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	dependencies = { "williamboman/mason.nvim" },
-	opts = {
-		ensure_installed = {
-			-- Formatters --https://github.com/stevearc/conform.nvim/tree/master/lua/conform/formatters
-			"prettierd", -- JS, TS, CSS, HTML
-			"stylua", -- Lua
-			"black", -- Python
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = { "williamboman/mason.nvim" },
+  opts = {
+    ensure_installed = {
+      -- Formatters --https://github.com/stevearc/conform.nvim/tree/master/lua/conform/formatters
+      "prettierd", -- JS, TS, CSS, HTML
+      "stylua", -- Lua
+      "black",  -- Python
 
-			-- Linters https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file to see all linters
-			"eslint_d", -- Javascript
-			"flake8", -- Python
-			"stylelint", -- CSS, SCSS linter
+      -- Linters https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file to see all linters
+      "eslint_d", -- Javascript
+      "flake8", -- Python
+      "stylelint", -- CSS, SCSS linter
 
-			-- LSPs https://mason-registry.dev/registry/list
-			"lua-language-server",
-			"css-lsp",
-			"typescript-language-server",
-			"emmet-ls",
-			"html-lsp",
-			"vue-language-server",
-			"tailwindcss-language-server",
-			"css-variables-language-server",
-			"cssmodules-language-server",
-		},
-		auto_update = true,
-		run_on_start = true,
-	},
-	config = function(_, opts)
-		require("mason-tool-installer").setup(opts)
-	end,
+      -- LSPs https://mason-registry.dev/registry/list
+      "lua-language-server",
+      "css-lsp",
+      "typescript-language-server",
+      "emmet-ls",
+      "html-lsp",
+      "svelte-language-server",
+      "vue-language-server",
+      "tailwindcss-language-server",
+      "css-variables-language-server",
+      "cssmodules-language-server",
+    },
+    auto_update = true,
+    run_on_start = true,
+  },
+  config = function(_, opts)
+    require("mason-tool-installer").setup(opts)
+  end,
 }
--- lua/plugins/none-ls-formatter.lua
+++ lua/plugins/none-ls-formatter.lua
@@ -1,25 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = { "williamboman/mason.nvim" },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettier,   -- JS, TS, CSS, HTML, etc.
-        null_ls.builtins.formatting.stylua,     -- Lua
-        -- Add more as needed
-      },
-      -- Optional: format on save
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = { "williamboman/mason.nvim" },
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = { -- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+				null_ls.builtins.formatting.prettierd, -- JS, TS, CSS, HTML, etc.
+				null_ls.builtins.formatting.stylua, -- Lua
+				-- Add more as needed
+			},
+			-- Optional: format on save
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
