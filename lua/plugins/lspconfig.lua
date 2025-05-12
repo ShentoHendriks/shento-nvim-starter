377a4b0c68d8327ffa21d45f9e6bf01a19c76494
Shento Hendriks <shentohendriks@gmail.com>
2025-05-12 14:22:46 +0200

    Now know the bug kinda behind svelte

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,51 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
+
+	-- example calling setup directly for each LSP
+	config = function()
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local lspconfig = require("lspconfig")
+
+		lspconfig["lua_ls"].setup({ capabilities = capabilities })
+		lspconfig["svelte"].setup({
+			capabilities = capabilities,
+		})
+		lspconfig["ts_ls"].setup({
+			capabilities = capabilities,
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescriptreact",
+				"javascript.jsx",
+				"typescript",
+				"typescript.tsx",
+			},
+		})
+		lspconfig["emmet_ls"].setup({
+			capabilities = capabilities,
+			filetypes = {
+				"html",
+				"typescriptreact",
+				"javascriptreact",
+				"css",
+				"sass",
+				"scss",
+				"less",
+				"svelte",
+				"javascript",
+			},
+		})
+		lspconfig["tailwindcss"].setup({
+			capabilities = capabilities,
+			filetypes = {
+				"svelte",
+				"vue",
+				"html",
+				"typescriptreact",
+				"javascriptreact",
+				"javascript",
+			},
+		})
+	end,
+}
